ba19d9064bc030e5b0676433d2eddbb514c4023f
CuiPaiPai <2459044496@qq.com>
2020-05-20 12:32:26 +0800

    修改排版，明确信息

--- src/com/cui/java8/InterfaceDefaultMethodTest.java
+++ src/com/cui/java8/InterfaceDefaultMethodTest.java
@@ -14,9 +14,9 @@ public class InterfaceDefaultMethodTest implements InterfaceDefaultMethod {
     /**
      * 解决默认方法冲突
      * 1，超类优先（一个超类，一个接口）
+     *   只考虑超类（一个超类，多个接口）
      * 2，接口冲突
      *      （多个接口）实现的两个接口方法冲突，必须覆写方法，解决二义性    接口名.super.方法名()
-     *      只考虑超类（一个超类，多个接口）
      * 不要让默认方法重新定义Object类中的某个方法，因为“类优先”规则，没有意思，所有类的超类都是Object
      */
 }
