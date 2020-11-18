1d861d543f8dab169c31d67168ace7eba12aa8f4
cuipai <2459044496@qq.com>
2020-11-18 11:06:41 +0800

    线程中断

---
+++ src/com/cui/bookreadingfeel/corejava/v1ch10/TeIO.java
@@ -0,0 +1,18 @@
+package com.cui.bookreadingfeel.corejava.v1ch10;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+/**
+ * @Desc:TODO
+ * @author: Cui Pai
+ * @date 2020/10/27 13:40
+ */
+public class TeIO {
+
+    public static void main(String[] args) throws Exception {
+        OutputStream outputStream = new FileOutputStream("");
+        outputStream.close();
+    }
+
+}
